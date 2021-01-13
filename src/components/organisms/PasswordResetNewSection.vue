6b69e96193f050b49bc224a31cff3fb4b9311c2b
Ryo Takahashi <ryogiftjp@gmail.com>
2021-01-13 20:23:15 +0900

    バリデーションエラーの表示修正

--- src/components/organisms/PasswordResetNewSection.vue
+++ src/components/organisms/PasswordResetNewSection.vue
@@ -1,11 +1,13 @@
 <template>
-  <div class="password-reset-section m">
+  <div class="password-reset-new-section m">
     <section class="section m-1 border-frame">
       <h1 class="title">パスワードのリセット</h1>
       <p>アカウントのメールアドレスをご入力ください。</p>
       <p>パスワードリセットのメールを送信します。</p>
       <form class="mt-1" @submit.prevent="handleSubmit">
-        <Input type="email" label="メールアドレス" v-model:value="email" />
+        <InputForm label="メールアドレス" type="email"
+                   :hasError="email.hasError" :errorMessages="email.errorMessages"
+                   v-model:value="email.value" />
         <SubmitButton class="mt-5 mb-4" label="送信" />
       </form>
       <InfoNotification v-if="infoMessage" :message="infoMessage" />
@@ -15,7 +17,7 @@
 </template>
 
 <script>
-import Input from '@/components/atoms/Input/Input.vue'
+import InputForm from '@/components/molecules/InputForm.vue'
 import SubmitButton from '@/components/molecules/SubmitButton.vue'
 import InfoNotification from '@/components/atoms/Notification/InfoNotification.vue'
 import ErrorNotification from '@/components/atoms/Notification/ErrorNotification.vue'
@@ -24,35 +26,48 @@ import { ReposiotryFactory } from './../../api/RepositoryFactory'
 const AuthRepository = ReposiotryFactory.get('auth')
 
 export default {
-  name: 'PasswordResetSection',
+  name: 'PasswordResetNewSection',
   components: {
-    Input,
+    InputForm,
     SubmitButton,
     InfoNotification,
     ErrorNotification
   },
   data () {
     return {
-      email: '',
+      email: {
+        value: '',
+        hasError: false,
+        errorMessages: []
+      },
       infoMessage: '',
       errorMessage: ''
     }
   },
   methods: {
     async handleSubmit () {
+      this.infoMessage = ''
+      this.errorMessage = ''
+      this.email.hasError = false
+      this.email.errorMessages = []
       try {
         const param = {
-          email: this.email
+          email: this.email.value
         }
         await AuthRepository.passwordResetsCreate(param)
         this.infoMessage = 'パスワードのリセットについてのメールを送信しました。'
-        this.email = ''
+        this.email.value = ''
       } catch (e) {
         const { status, data } = e.response
-        this.password = ''
-        const expectedStatuses = [401, 403, 423]
-        if (expectedStatuses.includes(status)) {
+        const expectedStatuses = [404]
+        if (expectedStatuses.includes(status) && data.error !== undefined) {
           this.errorMessage = data.error.message
+        } else if (status === 422) {
+          const { email } = data
+          if (email !== undefined) {
+            this.email.hasError = true
+            this.email.errorMessages = email
+          }
         } else {
           this.errorMessage = '予期せぬエラーです。'
         }
@@ -66,7 +81,7 @@ export default {
 .border-frame {
   border: 1px solid #c5c4c4;
   border-radius: 0.25rem;
-  height: 380px;
+  min-height: 380px;
 }
 .m {
   margin-top: 2rem;
