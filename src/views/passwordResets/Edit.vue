f874de66c53dd18887668d0c1146e3aa702810b8
Ryo Takahashi <info.ry0takahash1@gmail.com>
2021-01-25 08:03:25 +0900

    ファイル名の先頭を大文字から小文字に変更

---
+++ src/views/passwordResets/Edit.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="password-resets-edit">
+    <div class="container max-width is-max-desktop">
+      <PasswordResetEditSection />
+    </div>
+  </div>
+</template>
+
+<script>
+import PasswordResetEditSection from '@/components/organisms/PasswordResetEditSection.vue'
+
+export default {
+  name: 'PasswordResetEdit',
+  components: {
+    PasswordResetEditSection
+  },
+  mounted () {
+    document.title = 'パスワードのリセット | Ryo.gift'
+  }
+}
+</script>
+
+<style scoped>
+.max-width {
+  max-width: 600px;
+}
+</style>
