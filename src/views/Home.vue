9dae879a96d56fe196b4082da32ad851ffcba458
Ryo Takahashi <ryogiftjp@gmail.com>
2021-03-03 01:57:41 +0900

    ポートフォリオの記載を削除

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -17,43 +17,12 @@
       <section class="hero center">
         <div class="hero-body">
           <div class="container">
-            <h1 class="title left">ポートフォリオ</h1>
-            <p class="left">ポートフォリオとして、ユーザー登録、認証、投稿機能をSPA(Single Page Application)で開発しました。</p>
-            <p class="left">以下は今回、開発したアーキテクチャの構成です。</p>
-            <img src="@/assets/architecture.png" width="600" />
-            <p class="left">素早いリリースを目的として普段、業務で利用している開発言語やフレームワークを使って開発しています。</p>
-            <p class="left">フロントエンドの技術のみ、最近リリースされたVue.js 3を利用しています。</p>
-
             <h1 class="title mt-5 left">機能イメージ</h1>
             <p>ホーム(全ユーザーの投稿一覧)</p>
             <img class="mb-5" src="@/assets/home.png" width="600" border="1" />
 
             <p>投稿一覧(ユーザーごとの投稿一覧)</p>
             <img src="@/assets/posts.png" width="600" border="1" />
-            <p class="left mt-5">ユーザー登録をしなくても、ご確認いただけるようにユーザーを用意しました。</p>
-            <p class="left mb-3">動作確認にご活用ください。</p>
-
-            <table class="table is-bordered inline-block">
-              <thead>
-                <tr>
-                  <th>ユーザー</th>
-                  <th>email</th>
-                  <th>password</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <td>試用ユーザー1</td>
-                  <td>trial1@ryo.gift</td>
-                  <td>trial1trial1</td>
-                </tr>
-                <tr>
-                  <td>試用ユーザー2</td>
-                  <td>trial2@ryo.gift</td>
-                  <td>trial2trial2</td>
-                </tr>
-              </tbody>
-            </table>
           </div>
         </div>
       </section>
