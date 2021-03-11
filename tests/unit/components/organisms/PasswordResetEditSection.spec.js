3b75e7a85d2cf2fe58ede3245e17b1a8e0aff7ea
Ryo Takahashi <info.ry0takahash1@gmail.com>
2021-03-12 01:02:15 +0900

    テストコード修正

--- tests/unit/components/organisms/PasswordResetEditSection.spec.js
+++ tests/unit/components/organisms/PasswordResetEditSection.spec.js
@@ -1,6 +1,7 @@
 import { mount } from '@vue/test-utils'
 import flushPromises from 'flush-promises'
 import PasswordResetEditSection from '@/components/organisms/PasswordResetEditSection.vue'
+import AuthRepository from '@/api/AuthRepository'
 
 jest.mock('@/api/AuthRepository', () => ({
   passwordResetsUpdate: jest.fn()
@@ -54,5 +55,64 @@ describe('PasswordResetEditSection.vue', () => {
     await wrapper.get('.submit-button').trigger('submit.prevent')
     await flushPromises()
     expect(wrapper.get('.info-notification').text()).toBe('パスワードがリセットされました。')
+  })
+
+  test('APIエラー(パスワードまたはパスワードの確認が未入力)時にエラーメッセージが表示されること', async () => {
+    const error = new Error()
+    const errorMessage = 'パスワードまたはパスワードの確認が入力されていません。'
+    error.response = { status: 422, data: { error: { message: errorMessage } } }
+    AuthRepository.passwordResetsUpdate.mockRejectedValue(error)
+    const wrapper = mount(PasswordResetEditSection, {
+      global: {
+        mocks: { $route: mockRoute }
+      }
+    })
+    await wrapper.get('.submit-button').trigger('submit.prevent')
+    await flushPromises()
+    expect(wrapper.get('.error-notification').text()).toBe(errorMessage)
+  })
+
+  test('APIエラー(パスワードが6文字以下)時にエラーメッセージが表示されること', async () => {
+    const error = new Error()
+    const errorMessages = ['6文字以上でご入力ください。']
+    error.response = { status: 422, data: { password: errorMessages } }
+    AuthRepository.passwordResetsUpdate.mockRejectedValue(error)
+    const wrapper = mount(PasswordResetEditSection, {
+      global: {
+        mocks: { $route: mockRoute }
+      }
+    })
+    await wrapper.get('.submit-button').trigger('submit.prevent')
+    await flushPromises()
+    expect(wrapper.get('.error-messages').text()).toBe(errorMessages[0])
+  })
+
+  test('APIエラー(パスワードの再確認が6文字以下)時にエラーメッセージが表示されること', async () => {
+    const error = new Error()
+    const errorMessages = ['6文字以上でご入力ください。']
+    error.response = { status: 422, data: { passwordConfirmation: errorMessages } }
+    AuthRepository.passwordResetsUpdate.mockRejectedValue(error)
+    const wrapper = mount(PasswordResetEditSection, {
+      global: {
+        mocks: { $route: mockRoute }
+      }
+    })
+    await wrapper.get('.submit-button').trigger('submit.prevent')
+    await flushPromises()
+    expect(wrapper.get('.error-messages').text()).toBe(errorMessages[0])
+  })
+
+  test('APIエラー(予期せぬエラー)時にエラーメッセージが表示されること', async () => {
+    const error = new Error()
+    error.response = { status: 500 }
+    AuthRepository.passwordResetsUpdate.mockRejectedValue(error)
+    const wrapper = mount(PasswordResetEditSection, {
+      global: {
+        mocks: { $route: mockRoute }
+      }
+    })
+    await wrapper.get('.submit-button').trigger('submit.prevent')
+    await flushPromises()
+    expect(wrapper.get('.error-notification').text()).toBe('予期せぬエラーです。')
   })
 })
--- tests/unit/components/organisms/PostModal.spec.js
+++ tests/unit/components/organisms/PostModal.spec.js
@@ -1,10 +1,12 @@
 import { mount } from '@vue/test-utils'
 import PostModal from '@/components/organisms/PostModal.vue'
+import flushPromises from 'flush-promises'
+import UserPostsRepository from '@/api/UserPostsRepository'
 
-jest.mock('@/api/AuthRepository', () => ({
-  login: jest.fn(() => {
-    return { data: { id: 1, name: 'test', admin: false } }
-  })
+jest.mock('@/api/UserPostsRepository', () => ({
+  show: jest.fn(),
+  create: jest.fn(),
+  update: jest.fn()
 }))
 
 describe('PostModal.vue', () => {
@@ -14,5 +16,118 @@ describe('PostModal.vue', () => {
       props: { title }
     })
     expect(wrapper.get('.modal-card-title').text()).toBe(title)
+  })
+
+  test('投稿内容が設定されること', async () => {
+    const title = 'test'
+    const type = 'update'
+    const content = 'test content'
+    UserPostsRepository.show.mockResolvedValue({ data: { content } })
+    const wrapper = mount(PostModal, {
+      props: { title, type }
+    })
+    await flushPromises()
+    expect(wrapper.vm.$data.content.value).toBe(content)
+  })
+
+  test('投稿内容が登録できること', async () => {
+    const title = 'test'
+    const type = 'create'
+    const wrapper = mount(PostModal, {
+      props: { title, type }
+    })
+    wrapper.vm.$data.content.value = 'test content'
+    wrapper.find("form").trigger("submit.prevent")
+    await flushPromises()
+    expect(wrapper.emitted()).toHaveProperty('close')
+  })
+
+  test('投稿内容が更新できること', async () => {
+    const title = 'test'
+    const type = 'update'
+    const id = 1
+    const content = 'test content'
+    UserPostsRepository.show.mockResolvedValue({ data: { content } })
+    const wrapper = mount(PostModal, {
+      props: { title, type, id }
+    })
+    wrapper.find("form").trigger("submit.prevent")
+    await flushPromises()
+    expect(wrapper.emitted()).toHaveProperty('close')
+  })
+
+  test('APIエラー(401)時にエラーメッセージが表示されること', async () => {
+    const error = new Error()
+    const errorMessage = 'test'
+    error.response = { status: 401, data: { error: { message: errorMessage } } }
+    UserPostsRepository.create.mockRejectedValue(error)
+    const title = 'test'
+    const type = 'create'
+    const wrapper = mount(PostModal, {
+      props: { title, type }
+    })
+    wrapper.find("form").trigger("submit.prevent")
+    await flushPromises()
+    expect(wrapper.vm.$data.errorMessage).toBe(errorMessage)
+  })
+
+  test('APIエラー(403)時にエラーメッセージが表示されること', async () => {
+    const error = new Error()
+    const errorMessage = 'test'
+    error.response = { status: 403, data: { error: { message: errorMessage } } }
+    UserPostsRepository.create.mockRejectedValue(error)
+    const title = 'test'
+    const type = 'create'
+    const wrapper = mount(PostModal, {
+      props: { title, type }
+    })
+    wrapper.find("form").trigger("submit.prevent")
+    await flushPromises()
+    expect(wrapper.vm.$data.errorMessage).toBe(errorMessage)
+  })
+
+  test('APIエラー(423)時にエラーメッセージが表示されること', async () => {
+    const error = new Error()
+    const errorMessage = 'test'
+    error.response = { status: 423, data: { error: { message: errorMessage } } }
+    UserPostsRepository.create.mockRejectedValue(error)
+    const title = 'test'
+    const type = 'create'
+    const wrapper = mount(PostModal, {
+      props: { title, type }
+    })
+    wrapper.find("form").trigger("submit.prevent")
+    await flushPromises()
+    expect(wrapper.vm.$data.errorMessage).toBe(errorMessage)
+  })
+
+  test('APIエラー(422)時にエラーメッセージが表示されること', async () => {
+    const error = new Error()
+    const errorMessages = ['test']
+    error.response = { status: 422, data: { content: errorMessages } }
+    UserPostsRepository.create.mockRejectedValue(error)
+    const title = 'test'
+    const type = 'create'
+    const wrapper = mount(PostModal, {
+      props: { title, type }
+    })
+    wrapper.find("form").trigger("submit.prevent")
+    await flushPromises()
+    expect(wrapper.vm.$data.content.errorMessages[0]).toBe(errorMessages[0])
+  })
+
+  test('APIエラー(予期せぬエラー)時にエラーメッセージが表示されること', async () => {
+    const error = new Error()
+    const errorMessages = ['test']
+    error.response = { status: 500 }
+    UserPostsRepository.create.mockRejectedValue(error)
+    const title = 'test'
+    const type = 'create'
+    const wrapper = mount(PostModal, {
+      props: { title, type }
+    })
+    wrapper.find("form").trigger("submit.prevent")
+    await flushPromises()
+    expect(wrapper.vm.$data.errorMessage).toBe('予期せぬエラーです。')
   })
 })
