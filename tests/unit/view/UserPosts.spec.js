788e67c59670ba00a8315bf51a399485d6f0265c
Ryo Takahashi <info.ry0takahash1@gmail.com>
2021-03-14 14:31:22 +0900

    UserPosts.vueのユニットテストを追加

--- tests/unit/view/UserPosts.spec.js
+++ tests/unit/view/UserPosts.spec.js
@@ -10,7 +10,11 @@ jest.mock('@/api/UserPostsRepository', () => ({
         { id: 1, displayUpdatedAt: '2020/10/11 10:00:00', content: 'aaa' }
       ]
     }
-  })
+  }),
+  show: jest.fn(() => {
+    return { data: { id: 2, displayUpdatedAt: '2020/10/10 10:00:00', content: 'bbb' } }
+  }),
+  destroy: jest.fn()
 }))
 
 describe('UserPosts.vue', () => {
@@ -28,5 +32,157 @@ describe('UserPosts.vue', () => {
     await flushPromises()
     expect(wrapper.html()).toContain('bbb')
     expect(wrapper.html()).toContain('aaa')
+  })
+
+  test('投稿の作成用モーダルウィンドウが表示されること', async () => {
+    const $store = {
+      state: { user: { name: 'test', admin: false } },
+      getters: { isCurrentUser: state => state.user.name !== '' }
+    }
+    const wrapper = mount(UserPosts, {
+      global: {
+        stubs: ['font-awesome-icon'],
+        mocks: { $store }
+      }
+    })
+    await flushPromises()
+    await wrapper.get('.create-button').trigger('click')
+    expect(wrapper.vm.$data.postModal).toBe(true)
+  })
+
+  test('投稿の作成用モーダルウィンドウで閉じるボタンクリック後にモーダルウィンドウが閉じること', async () => {
+    const $store = {
+      state: { user: { name: 'test', admin: false } },
+      getters: { isCurrentUser: state => state.user.name !== '' }
+    }
+    const wrapper = mount(UserPosts, {
+      global: {
+        stubs: ['font-awesome-icon'],
+        mocks: { $store }
+      }
+    })
+    await flushPromises()
+    await wrapper.get('.create-button').trigger('click')
+    await wrapper.get('.delete').trigger('click')
+    expect(wrapper.vm.$data.postModal).toBe(false)
+  })
+
+  test('投稿の編集用モーダルウィンドウが表示されること', async () => {
+    const $store = {
+      state: { user: { name: 'test', admin: false } },
+      getters: { isCurrentUser: state => state.user.name !== '' }
+    }
+    const wrapper = mount(UserPosts, {
+      global: {
+        stubs: ['font-awesome-icon'],
+        mocks: { $store }
+      }
+    })
+    await flushPromises()
+    await wrapper.get('.dropdown-menu').trigger('click')
+    await wrapper.get('.edit-link-button').trigger('click')
+    expect(wrapper.vm.$data.postModal).toBe(true)
+  })
+
+  test('投稿の編集用モーダルウィンドウで閉じるボタンクリック後にモーダルウィンドウが閉じること', async () => {
+    const $store = {
+      state: { user: { name: 'test', admin: false } },
+      getters: { isCurrentUser: state => state.user.name !== '' }
+    }
+    const wrapper = mount(UserPosts, {
+      global: {
+        stubs: ['font-awesome-icon'],
+        mocks: { $store }
+      }
+    })
+    await flushPromises()
+    await wrapper.get('.dropdown-menu').trigger('click')
+    await wrapper.get('.edit-link-button').trigger('click')
+    await wrapper.get('.delete').trigger('click')
+    expect(wrapper.vm.$data.postModal).toBe(false)
+  })
+
+  test('削除確認モーダルウィンドウが表示されること', async () => {
+    const $store = {
+      state: { user: { name: 'test', admin: false } },
+      getters: { isCurrentUser: state => state.user.name !== '' }
+    }
+    const wrapper = mount(UserPosts, {
+      global: {
+        stubs: ['font-awesome-icon'],
+        mocks: { $store }
+      }
+    })
+    await flushPromises()
+    await wrapper.get('.remove-link-button').trigger('click')
+    expect(wrapper.vm.$data.removeModal).toBe(true)
+  })
+
+  test('削除確認モーダルウィンドウで削除ボタンクリック後にモーダルウィンドウが閉じること', async () => {
+    const $store = {
+      state: { user: { name: 'test', admin: false } },
+      getters: { isCurrentUser: state => state.user.name !== '' }
+    }
+    const wrapper = mount(UserPosts, {
+      global: {
+        stubs: ['font-awesome-icon'],
+        mocks: { $store }
+      }
+    })
+    await flushPromises()
+    await wrapper.get('.remove-link-button').trigger('click')
+    await wrapper.get('.remove-button').trigger('click')
+    await flushPromises()
+    expect(wrapper.vm.$data.removeModal).toBe(false)
+  })
+
+  test('ドロップダウンメニューが表示できること', async () => {
+    const $store = {
+      state: { user: { name: 'test', admin: false } },
+      getters: { isCurrentUser: state => state.user.name !== '' }
+    }
+    const wrapper = mount(UserPosts, {
+      global: {
+        stubs: ['font-awesome-icon'],
+        mocks: { $store }
+      }
+    })
+    await flushPromises()
+    await wrapper.get('.ellipsis-button').trigger('click')
+    expect(wrapper.vm.$data.isDropDown).toBe(true)
+  })
+
+  test('ドロップダウンメニューが閉じること', async () => {
+    const $store = {
+      state: { user: { name: 'test', admin: false } },
+      getters: { isCurrentUser: state => state.user.name !== '' }
+    }
+    const wrapper = mount(UserPosts, {
+      global: {
+        stubs: ['font-awesome-icon'],
+        mocks: { $store }
+      }
+    })
+    await flushPromises()
+    await wrapper.get('.ellipsis-button').trigger('click')
+    await wrapper.get('.ellipsis-button').trigger('click')
+    expect(wrapper.vm.$data.isDropDown).toBe(false)
+  })
+
+  test('ドロップダウンメニューが全て閉じること', async () => {
+    const $store = {
+      state: { user: { name: 'test', admin: false } },
+      getters: { isCurrentUser: state => state.user.name !== '' }
+    }
+    const wrapper = mount(UserPosts, {
+      global: {
+        stubs: ['font-awesome-icon'],
+        mocks: { $store }
+      }
+    })
+    await flushPromises()
+    await wrapper.get('.ellipsis-button').trigger('click')
+    await wrapper.get('.dropdown-background').trigger('click')
+    expect(wrapper.vm.$data.isDropDown).toBe(false)
   })
 })
